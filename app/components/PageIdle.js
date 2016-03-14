7a8002c164a9eae3f5df1b8b9bec376b6ab028d0
kfancy-sdk <kent@sdkcore.com>
2016-03-14 10:20:09 -0700

    Create PageIdle.js

---
+++ app/components/PageIdle.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import RootscopeActions from '../actions/RootscopeActions'
+import TsvService from '../lib/TsvService'
+import RootscopeStore from '../store/RootscopeStore'
+
+class Page_Idle extends Component {
+
+  constructor(props, context) {
+  	// MUST call super() before any this.*
+  	super(props, context);
+    
+    RootscopeActions.setConfig('bDisplayCgryNavigation', false);
+    RootscopeActions.setConfig('bShowCredit', RootscopeStore.getCache('credit') && true);
+    
+    TSVService.disableLoginDevices();
+  }
+
+  getInitialState() {
+    return {};
+  }
+
+  // Add change listeners to stores
+  componentDidMount() {
+  }
+
+  // Remove change listers from stores
+  componentWillUnmount() {
+  }
+
+  render() {
+    return (
+      <div className="Page_Idle" onClick={this.idleClicked}>
+      	<img id="idleImg" ng-src="{{localizedImage('idle.png')}}" alt="IdlePage" />
+      </div>
+    );
+  }
+  
+  idleClicked(e) {
+  	e.preventDefault();
+  	// probably triggers a route change, according to the current TSVService func
+  	TSVService.idleClicked();
+  }
+
+});
+
+export default Page_Idle
