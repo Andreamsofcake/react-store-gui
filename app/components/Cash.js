5ae41c806438ddd860dc28c43236061e1152e566
“Andreamsofcake” <andrea.moulding@gmail.com>
2016-03-16 12:06:53 -0700

    cash commonent third pass

--- app/components/Cash.js
+++ app/components/Cash.js
@@ -5,6 +5,7 @@ import * as Translate from '../lib/Translate'
 import RootscopeActions from '../actions/RootscopeActions'
 import RootscopeStore from '../store/RootscopeStore'
 import browserHistory from 'react-router'
+import * as _E from 'elemental'
 
 class Cash extends Component {
 
@@ -12,11 +13,72 @@ class Cash extends Component {
     // MUST call super() before any this.*
     super(props, context);
 
+    RootscopeActions.setSession('currentView', 'Cash');
+    TsvService.enablePaymentDevice()
+    this.state = {
+      insertedAmount: RootscopeStore.getSession('inserted')
+      summary: RootscopeStore.getCache('shoppingCart.summary'),
+      hintMsg: "Insert Cash Now....",
+      salesTaxAmount: RootscopeStore.getCache('shoppingCart.summary.salesTaxAmount'),
+      showCancelBtnCash: true,
+      summary: RootscopeStore.getCache('shoppingCart.summary'),
+      item: RootscopeStore.getCache('shoppingCart.detail')[0]
+    };
+      TsvService.startPaymentTimer();
+
+    if(RootscopeStore.getSession('bVendingInProcess')){
+
+        this.state.showSpinner = true;
+        TSVService.stopPaymentTimer();
+        this.state.hintMsg = "Vending...";
+        this.state.showCancelBtnCash = false;
+      }
+
   }
-  function loadingSpinner(){ console.warn(‘spinner called for, but we need elemental’) }
+
+
+  cancel(){
+    TsvService.emptyCart();
+    this.insertedAmount = 0;
+    stopPaymentTimer();
+    browserHistory.push("/view1");
+  }
+
+  checkBalance(){
+      var total = RootscopeStore.getCache('shoppingCart.summary.TotalPrice');
+
+      if((this.insertedAmount * 100) >= (total * 100) && RootscopeStore.getCache('shoppingCart.detail').length > 0){
+          TSVService.disablePaymentDevice();
+          if(!RootscopeStore.getSession('bVendingInProcess')){
+              TsvService.startVend();
+              RootscopeActions.setSession('bVendingInProcess', true);
+
+              this.setState({
+                hintMsg: "Vending...",
+                showCancelBtnCash: false,
+                showSpinner: true
+              });
+          }
+          return 1;
+      }
+      return -1;
+  }
 
   // Add change listeners to stores
   componentDidMount() {
+
+    TvsService.subscribe("creditBalanceChanged", (ins, balance) => {
+        if(RootscopeStore.getSession('currentView') != "Cash")    return;
+
+        RootscopeActions.setSession('inserted', balance/100.00);
+        RootscopeActions.setSession('creditBalance', this.state.summary.TotalPrice - balance/100.00);
+
+        this.checkBalance();
+        TsvService.resetPaymentTimer();
+        this.setState({
+            insertedAmount: balance/100.00
+        });
+    });
   }
 
   // Remove change listers from stores
@@ -40,7 +102,7 @@ class Cash extends Component {
 
         { if (this.state.showCancelBtnCash) { this.renderCancelBtnCash()} }
 
-        <canvas id="spinner"></canvas>
+        { if (this.state.showSpinner) { this.renderSpinner()} }
 
       </div>
     );
@@ -49,6 +111,12 @@ class Cash extends Component {
   renderCancelBtnCash(){
     return(
       <img id="cancelImg" src="../Images/cancel.png" onClick={this.cancel()} />
+    )
+  }
+
+  renderSpinner(){
+    return(
+      <_E.Spinner size="md" type="inverted"   />
     )
   }
 
