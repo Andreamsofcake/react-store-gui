19c132364b6ed6b9a917555f9283ddb39f072694
“Andreamsofcake” <andrea.moulding@gmail.com>
2016-03-16 16:18:23 -0700

    card vending component second pass

--- app/components/CardVending.js
+++ app/components/CardVending.js
@@ -22,6 +22,7 @@ class Card_Vending extends Component {
       item: RootscopeStore.getCache('shoppingCart.detail')[0],
       summary: RootscopeStore.getCache('shoppingCart.summary'),
       showCancelBtn: true
+      cardTransactionResponse: Translate.translate('Card_Vending', 'InstructionMessage')
     };
       TsvService.resetPaymentTimer();
 
@@ -29,7 +30,13 @@ class Card_Vending extends Component {
       && RootscopeStore.getSession('cardMsg')!= Translate.translate('VendingMessage')
       && RootscopeStore.getSession('cardMsg')!= Translate.translate('InstructionMessage')
       ){
-        TsvService.startCardErrorTimer();
+      TsvService.startCardErrorTimer();
+       }
+
+     if(RootscopeStore.getSession('bVendingInProcess')){
+         this.state.showSpinner = true;
+         this.state.cardTransactionResponse = Translate.translate('Card_Vending', "VendingMessage");
+         this.state.showCancelBtnCash = false;
        }
 
   }
@@ -41,41 +48,107 @@ class Card_Vending extends Component {
     TsvService.gotoDefaultIdlePage();
   }
 
-  checkBalance(){
-      var total = RootscopeStore.getCache('shoppingCart.summary.TotalPrice');
-
-      if((this.insertedAmount * 100) >= (total * 100) && RootscopeStore.getCache('shoppingCart.detail').length > 0){
-          TSVService.disablePaymentDevice();
-          if(!RootscopeStore.getSession('bVendingInProcess')){
-              TsvService.startVend();
-              RootscopeActions.setSession('bVendingInProcess', true);
-
-              this.setState({
-                hintMsg: "Vending...",
-                showCancelBtnCash: false,
-                showSpinner: true
-              });
-          }
-          return 1;
-      }
-      return -1;
-  }
-
   // Add change listeners to stores
   componentDidMount() {
+      var cardTransactionHandler = function(level) {
+          TsvService.killCardErrorTimer();
+          TsvService.resetPaymentTimer();
 
+            if(!RootscopeStore.getSession('bVendingInProcess'){
+                switch(level){
+                    case "CARD_INSERTED":
+                        this.state.cardTransactionResponse = Translate.translate("ProcessingMessage");
+                        break;
+                    case "CARD_PROCESSING":
+                        this.state.cardTransactionResponse = Translate.translate("ProcessingMessage");
+                        break;
+                    case "CARD_APPROVED":
+                        startVend();
+                        break;
+                    case "CARD_INVALID_READ":
+                        this.state.cardTransactionResponse = Translate.translate("CardInvalidMessage");
+                        TsvService.startCardErrorTimer();
+                        break;
+                    case "CARD_DECLINED":
+                        this.state.cardTransactionResponse =  Translate.translate("CardDeclinedMessage");
+                        TsvService.startCardErrorTimer();
+                        break;
+                    case "CARD_CONNECTION_FAILURE":
+                        this.state.cardTransactionResponse = Translate.translate("CardConnectionErrorMessage");
+                        TsvService.startCardErrorTimer();
+                        break;
+                    case "CARD_UNKNOWN_ERROR":
+                        this.state.cardTransactionResponse = Translate.translate("CardUnknownErrorMessage");
+                        TsvService.startCardErrorTimer();
+                        break;
+                    default:
+                        console.log("Card_Vending Got event cardTransactionResponse()default: "+level);
+                        this.state.cardTransactionResponse = Translate.translate("ErrorMessage");
+                        TsvService.startCardErrorTimer();
+                        break;
+        }
+      }
+    };
+    TsvService.subscribe("cardTransactionResponse", cardTransactionHandler, "app.cardVending");
+    TsvService.subscribe("vendResponse", vendResponseHandler, "app.cardVending");
   }
 
   // Remove change listers from stores
   componentWillUnmount() {
+    TsvService.unsubscribe("cardTransactionResponse", "app.cardVending");
+    TsvService.unsubscribe("vendResponse", "app.cardVending");
   }
 
   render() {
     return (
       <div className="Card_Vending" >
         <h2>{Translate.translate('Card_Vending', 'CardVending')}</h2>
+        <table className="cart">
+
+            <tr>
+              {cart.map((prd, $index) => {
+                return(
+                  <td key={$index}>
+
+                      <img id="prdImg" src={ prd.imagePath } alt="productImage"/> {/*err-src="../Images/ProductImageNotFound.png"*/}
+
+                  </td>
+                )}
+              )}
+
+            </tr>
+
+        </table>
+
+        { if (this.state.summary.TotalPrice >= 1) { this.renderTotalPriceLabel()} }
+
+        <p id = "cardResponse">{ this.state.cardTransactionResponse }</p>
+
+        <img id="creditCards" src="../Images/creditcards.png" alt="creditcards">
+
+        { if (this.state.showCancelBtnCash) { this.renderCancelBtnCash()} }
+
+        { if (this.state.showSpinner) { this.renderSpinner()} }
       </div>
     );
+  }
+
+  renderCancelBtnCash(){
+    return(
+      <img id="cancelImg" src="../Images/cancel.png" onClick={this.cancel()} />
+    )
+  }
+
+  renderTotalPriceLabel() {
+    return(
+      <p { Translate.translate('Card_Vending', 'TotalPriceLabel')}{TsvService.currencyFilter(this.summary.TotalPrice) }</p>
+    )
+  }
+
+  renderSpinner(){
+    return(
+      <_E.Spinner size="md" type="inverted"   />
+    )
   }
 
 }
