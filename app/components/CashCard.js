84177f6afb35fb13b928b069785f9220c22b033c
“Andreamsofcake” <andrea.moulding@gmail.com>
2016-03-17 12:43:06 -0700

    cashcard first pass

---
+++ app/components/CashCard.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import TsvService from '../lib/TsvService'
+import * as Translate from '../lib/Translate'
+
+import RootscopeActions from '../actions/RootscopeActions'
+import RootscopeStore from '../stores/RootscopeStore'
+import browserHistory from 'react-router'
+
+class Cash_Card extends Component {
+
+  constructor(props, context) {
+    // MUST call super() before any this.*
+    super(props, context);
+
+    RootscopeActions.setSession('currentView', 'Cash_Card');
+    this.state = {
+      activationKey: '',
+      serialNumber: TsvService.machineSetting('MachineSerialNumber')
+    };
+
+  // Add change listeners to stores
+  componentDidMount() {
+    TsvService.subscribe("cardTransactionResponse",
+      (level) => {
+        if(!RootscopeStore.getSession('bVendingInProcess')) {
+            if(browserHistory.push() != "/Card_Vending"){
+                browserHistory.push("/Card_Vending");
+            }
+
+            TsvService.cardTransaction(level);
+        }
+    }
+    , "app.cashCard");
+  }
+
+  // Remove change listers from stores
+  componentWillUnmount() {
+    TsvService.unsubscribe("cardTransactionResponse", "app.cashCard");
+  }
+
+  render() {
+    return (
+      <div className="Cash_Card">
+      <h2>{Translate.translate('Cash_Card', 'InstructionMessage')}</h2>
+
+      <div className="cashAndCards">
+
+          <img className="paymentMethod" onClick={this.cash()} src={Translate.localizedImage('cash.png')} alt="cash">
+
+          <img className="paymentMethod" onClick={this.card()} src={Translate.localizedImage('card.png')} alt="card">
+
+      </div>
+
+      { if (this.state.showCancelBtnCash) { this.renderCancelBtnCash()} }
+
+      </div>
+    );
+  }
+
+  renderCancelBtnCash(){
+    return(
+      <img id="cancelImg" src="../Images/cancel.png" onClick={this.cancel()} />
+    )
+  }
+
+}
+
+export default Activate
