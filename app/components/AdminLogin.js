e88287d6e9b309c40d74eddea8b43afb453db486
“Andreamsofcake” <andrea.moulding@gmail.com>
2016-03-22 12:10:12 -0700

    AdminLogin

--- app/components/AdminLogin.js
+++ app/components/AdminLogin.js
@@ -7,18 +7,61 @@ import RootscopeStore from '../stores/RootscopeStore'
 import browserHistory from 'react-router'
 import * as _E from 'elemental'
 
-class AdminLogin extends Component {
+class Admin_Login extends Component {
 
   constructor(props, context) {
     // MUST call super() before any this.*
     super(props, context);
 
-    RootscopeActions.setSession('currentView', 'AdminLogin');
+    RootscopeActions.setSession('currentView', 'Admin_Login');
+    TsvService.disableLoginDevices();
+    TsvService.emptyCart();
     this.state = {
-
+      num: "",
+      maxChars: 6,
+      instructionMessage: Translate.translate('Admin_Login','Password')
     };
+  }
 
-  // Add change listeners to stores
+  enter() {
+    var res = RootscopeStore.getConfig('validateAdminPassword', this.state.num); //unsure about get call
+
+    switch(res.result){
+        case "VALID":
+            browserHistory.path("/Admin_Home");
+            break;
+        default:
+            this.state.instructionMessage = Translate.translate('Admin_Login', 'InvalidPassword'); //"Invalid Password";
+            break;
+    }
+
+  }
+
+  clear() {
+    this.state.instructionMessage = Translate.translate('Admin_Login', 'Password')
+    this.state.num = ""
+  }
+
+  press() {
+    if(this.state.num.length < this.state.maxChars){
+        this.state.num += digit;  //What?
+    }else{
+        this.state.num = "";
+        this.state.num += digit;
+    }
+  }
+
+  prompt(){
+    if(this.state.num.length != 0){
+        return this.state.num;
+    }
+    return "";
+  }
+
+  back() {
+    browserHistory.push("/view1");
+  }
+    // Add change listeners to stores
   componentDidMount() {
   }
 
@@ -28,37 +71,41 @@ class AdminLogin extends Component {
 
   render() {
     return (
-      <_E.Row className="AdminLogin" >
+      <_E.Row className="Admin_Login" >
+        <_E.Col>
 
-          <h2 id="instruction">{{ instructionMessage }}</h2>
+          <_E.h2 id="instruction">{ this.instructionMessage }</_E.h2>
 
-          <img className="regularBtn" id="back" ng-src="{{localizedImage('back.png')}}" err-src="../Images/back.png" ng-click="back()">
+          <_E.Button id="back" onClick={this.back}>Back</_E.Button>
 
-          <div id="keypad">
+          <_E.Row>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(1)}>1</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(2)}>2</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(3)}>3</_E.Button></_E.Col>
+          </_E.Row>
+          <_E.Row>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(4)}>4</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(5)}>5</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(6)}>6</_E.Button></_E.Col>
+          </_E.Row>
+          <_E.Row>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(7)}>7</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(8)}>8</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(9)}>9</_E.Button></_E.Col>
+          </_E.Row>
+          <_E.Row>
+              <_E.Col basis="1/4"><_E.Button type="warning" onClick={this.clear}>Clear</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button  onClick={this.press(0)}>0</_E.Button></_E.Col>
+              <_E.Col basis="1/4"><_E.Button type="primary" onClick={this.enter}>Enter</_E.Button></_E.Col>
+          </_E.Row>>
 
-              <img src="../Images/Button_1.png" ng-click="press(1)">
-              <img src="../Images/Button_2.png" ng-click="press(2)">
-              <img src="../Images/Button_3.png" ng-click="press(3)">
-              <img src="../Images/Button_4.png" ng-click="press(4)">
-              <img src="../Images/Button_5.png" ng-click="press(5)">
-              <img src="../Images/Button_6.png" ng-click="press(6)">
-              <img src="../Images/Button_7.png" ng-click="press(7)">
-              <img src="../Images/Button_8.png" ng-click="press(8)">
-              <img src="../Images/Button_9.png" ng-click="press(9)">
-              <img src="../Images/Button_10.png" ng-click="press(0)">
+          <input id="coilInput" type="password" value={this.prompt()} onChange={this.num.bind(this)}> //unsure
 
-              <img ng-src="{{localizedImage('Button_Clear.png')}}" err-src="../Images/Button_Clear.png" ng-click="clear()">
-              <img ng-src="{{localizedImage('Button_Enter.png')}}" err-src="../Images/Button_Enter.png" ng-click="enter()">
-
-          </div>
-
-          <input id="coilInput" type="password" value={{prompt()}}>
-
-
+          </_E.Col>
       </_E.Row>
 
 
-      /*<div className="AdminLogin" >
+      /*<div className="Admin_Login" >
 
           <h2 id="instruction">{{ instructionMessage }}</h2>
 
