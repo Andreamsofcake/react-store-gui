b623d99c60d5e552455d5153f9a683fe6500d39f
kfancy-sdk <kent@sdkcore.com>
2016-07-14 18:01:46 -0700

    small fix to app idle

--- app/components/App.js
+++ app/components/App.js
@@ -121,7 +121,7 @@ class App extends Component {
 	_onTsvChange(event) {
 		if (event && event.method === 'notifyTSVReady') {
 			Big.log("Got event notifyTSVReady, what is our current location? " + TsvSettingsStore.getCache('currentLocation'));
-			if (TsvSettingsStore.getCache('currentLocation') === '/View0') {
+			if (TsvSettingsStore.getCache('currentLocation') === '/View0' || !TsvSettingsStore.getCache('currentLocation')) {
 				Big.log("Redirect to default idle page or reload");
 
 				if (!TsvSettingsStore.getCache('custommachinesettings')) {
